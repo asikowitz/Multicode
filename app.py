1350a1a99ab6ea0c9dfeeabb2d790a80d25ea624
Oliver Zhang <oliverzhang@olivers-mbp.home>
2015-05-05 21:48:15 -0400

    mongo probs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 import json
+import db
 
 app = Flask(__name__)
 mongo = MongoClient()
@@ -8,13 +9,13 @@ db = mongo['filesdb']
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
-
+    if request.method=="GET":
+        return render_template("index.html");
+    
 if __name__ == "__main__":
-   app.debug = True
-   app.run(host="0.0.0.0",port=5678)
-   
+    app.debug = True
+    app.run(host="0.0.0.0",port=5678)
+        
 @app.route("/ufile",methods=['GET','POST','DELETE','PUT'])
 @app.route("/ufile/<id>",methods=['GET','POST','DELETE','PUT'])
 def ufile(id=None):
@@ -37,4 +38,4 @@ def ufile(id=None):
         x = db.notes.remove({'_id':id})
 
     return json.dumps({'result':x})
->>>>>>> 04430243bc10d410e900452d4206abb82c7ac536
+
