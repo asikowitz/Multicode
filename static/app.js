cc8ce46cbcfd23890b7447060a178f32a01cc4f1
asikowitz <nysiko@gmail.com>
2015-05-25 20:38:35 -0400

    Stuff

--- static/app.js
+++ static/app.js
@@ -1,6 +1,11 @@
 console.log("HELLO");
 
 var app = new Marionette.Application();
+var refresh_var;
+
+var refresh = function() {
+    app.cur.fetch({data:app.cur.toJSON(),processData:true});
+};
 
 app.addRegions({
     main:"#main",
@@ -8,12 +13,15 @@ app.addRegions({
     second:"#second",
 });
 
+app.main.on("show", function() {
+    app.editor = CodeMirror.fromTextArea(document.getElementById("textarea"),{mode:"htmlmixed"});
+    app.editor.getDoc().setValue(app.cur.get("content"));
+});
 
 app.on("start",function() {
     console.log("Started");
     console.log(filename,username);
-    app.c = new app.Collection({user:username,project:project});
-    console.log("BRO",app.c);
+    app.c = new app.Collection([],{user:username,project:project});
     if (filename != "None" && username != "None" && project != "None") {
 	console.log("FETCHING");
 	app.cur = new app.File({name:filename,user:username,project:project});
@@ -26,14 +34,15 @@ app.on("start",function() {
 			   app.fv = new app.FileView({model:app.cur});
 			   page = new app.Page({content:app.fv.model.get("content")});
 			   app.pv = new app.PageView({model:page});
+			   app.cv = new app.CollectionView({collection:app.c})
 			   app.main.show(app.fv);
-			   //app.first.show(app.cv);
+			   app.first.show(app.cv);
 			   app.second.show(app.pv);
+			   refresh_var = setInterval(refresh, 100);
 		       }});
     }
-    //If there is no filename, need to fetch collection
-    //Before getting file
-    Backbone.history.start()
+    //If there is no filename, need to fetch collection before getting file
+    //Backbone.history.start()
 });
 
 app.File = Backbone.Model.extend({
@@ -41,43 +50,62 @@ app.File = Backbone.Model.extend({
     idAttribute:'_id',
     id:'_id',
     initialize : function() {
-	console.log(this.id);
+	that = this;
+	this.on({
+	    "change":function() {
+		//$("#txt-box").val(that.get("content"));
+		var cursor = app.editor.getCursor();
+		app.editor.setValue(that.get("content"));
+		app.editor.setCursor(cursor);
+		app.pv.model.set({content:that.get("content")});
+		app.pv.render();
+	    }
+	})
     }
 });
 
 app.Collection = Backbone.Collection.extend({
     model:app.File,
     url:'/files',
-    initialize : function() {
-	that = this;
-	console.log("YO",this.toJSON());
-	this.fetch({data:this.toJSON(),processData:true,
+    initialize : function(models,options) {
+	this.user = options.user;
+	this.project = options.project;
+	this.fetch({data:{'user':this.user,'project':this.project},processData:true,
 		    error:function(d) {
 			console.log("ERROR",d);
 		    },
 		    success:function(d) {
 			console.log("Fetched",d);
-			if (that.length > 0) {
-			    if (filename == "None") {
+			if (filename == "None") { //If no filename specified
+			    if (app.c.length > 0) {
 				console.log("Defaulting to first file");
-				app.fv = new app.FileView({model:app.c.at(0)});
+				app.cur = app.c.at(0);
+				app.fv = new app.FileView({model:app.cur});
 				page = new app.Page({content:app.fv.model.get("content")});
 				app.pv = new app.PageView({model:page});
+				app.cv = new app.CollectionView({collection:app.c})
 				app.main.show(app.fv);
-				//app.first.show(app.cv);
+				app.first.show(app.cv);
 				app.second.show(app.pv);
+				var refresh_var = setInterval(refresh, 100);
 			    }
-			}
-			else {
-			    console.log(filename);
-			    if (filename == "None") {
-				console.log("Making first files.");
-				var f = new app.File({name:"First",content:"Start writing here.",project:project,user:username});
-				f.save(f.toJSON());
+			    else {
+				window.location.assign("/newfile/"+project);
 			    }
 			}
 		    }});
     }
+});
+
+app.FV = Marionette.ItemView.extend({
+    template : "<script type='text/template'><%- name %></script>", //"#file-template",
+    model : app.File,
+    tagName : "li"
+});
+
+app.CollectionView = Marionette.CollectionView.extend({
+    childView : app.FV,
+    tagName : "ul"
 });
 
 app.Page = Backbone.Model.extend({});
@@ -89,32 +117,34 @@ app.PageView = Marionette.ItemView.extend({
 /*app.FV = Marionette.ItemView.extend({
   tagName:'li',
   template:"<%= name %>",
-});
+  });
 
-app.CV = Marionette.CollectionView.extend({
-    el:"ul",
-    childView:app.FV,
-});*/
+  app.CV = Marionette.CollectionView.extend({
+  el:"ul",
+  childView:app.FV,
+  });*/
 
 app.FileView = Marionette.ItemView.extend({
     template : "#edit-template",
     model : app.File,
     events : {
-	"click #edit" : function() {
-	    $("#edit").prop("disabled",true);
-	    $("#read").prop("disabled",false);
-	    $("#txt-box").css("display","inline-block");
-	},
-	"click #read" : function() {
-	    $("#edit").prop("disabled",false);
-	    $("#read").prop("disabled",true);
-	    $("#txt-box").css("display","none");
-	},
-	"keyup #txt-box" : function(e) {
-	    this.model.set({content:$("#txt-box").val()});
+	/*"click #edit" : function() {
+	  $("#edit").prop("disabled",true);
+	  $("#read").prop("disabled",false);
+	  $("#txt-box").css("display","inline-block");
+	  },
+	  "click #read" : function() {
+	  $("#edit").prop("disabled",false);
+	  $("#read").prop("disabled",true);
+	  $("#txt-box").css("display","none");
+	  },*/
+	"keyup .CodeMirror" : function(e) {
+	    //window.clearInterval(refresh_var);
+	    this.model.set({content:app.editor.getValue()});
 	    this.model.save(this.model.toJSON(),{success:function(){}});
-	    app.pv.model.set({content:$("#txt-box").val()});
-	    app.pv.render();
+	    //app.pv.model.set({content:$("#txt-box").val()});
+	    //app.pv.render();
+	    //refresh_var = window.setInterval(refresh,100);
 	},
 	"click #delete" : function() {
 	    this.remove();
